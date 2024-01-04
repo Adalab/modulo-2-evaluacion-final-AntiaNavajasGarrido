e024c7ed987da5c5885ef8e67721b3ce43a8e90b
AntiaNavajasGarrido <antianavajasgarrido@gmail.com>
2024-01-04 21:38:30 +0100

    changes in SCSS, HTML and JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//NO ENTIENDO FETCH MIDU SALVAME
+
+fetch('api.disneyapi.dev/character?pageSize=50')
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        data.forEach(disneyCharacter => {
+            const characters = `<li>${disneyCharacter.name}</li>`;
+
+            document.querySelector('ul').insertAdjacentHTML('beforeend', characters);
+        })
+    })
+    .catch(error => console.log(error));
