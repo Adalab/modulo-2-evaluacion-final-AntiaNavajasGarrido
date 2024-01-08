50a793a63499122f598e22b46cf72c924cb60416
AntiaNavajasGarrido <antianavajasgarrido@gmail.com>
2024-01-08 18:48:32 +0100

    deploy doc files

--- src/js/main.js
+++ src/js/main.js
@@ -6,10 +6,11 @@ console.log('>> Ready :)');
 // EVENTO PARA CARGAR TODOS LOS PERSONAJES
 
 document.addEventListener('DOMContentLoaded', loadCharacters);
-
+const favoriteCharacters = [];
+const characterCards = document.querySelectorAll('.characterCard');
 
 async function loadCharacters() {
-    const apiUrl = '//api.disneyapi.dev/character';
+    const apiUrl = '//api.disneyapi.dev/character?pageSize=50';
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
@@ -30,7 +31,7 @@ function showCharactersInHTML(characters) {
 
     characters.forEach(character => {
         //USO DEL OPERADOR ? PARA SABER SI TIENE IMAGEN, SI NO CARGARA LA ESPECIFICADA EN EL EJERCICIO - DISAPPOINTED BUT NOT SURPRISED: NO ME FUNCIONA
-        // const imageUrl = personaje.imageUrl ? personaje.imageUrl : 'https://via.placeholder.com/210x295/ffffff/555555/?text=Disney';
+        // const imageUrl = character.imageUrl ? character.imageUrl : 'https://via.placeholder.com/210x295/ffffff/555555/?text=Disney';
         const characterHTML = `
       <div class="characterCard">
         <h3 class="characterName">${character.name}</h3>
@@ -45,13 +46,39 @@ function showCharactersInHTML(characters) {
 
 //FUNCIONES PARA MOSTRAR LOS FAVORITOS
 
+
+characterCards.forEach(card => {
+  card.addEventListener('click', () => {
+    addToFavorites(card);
+  });
+});
+
+function addToFavorites(card) {
+const characterName = card.querySelector('.characterName').textContent;
+const isAlreadyFavorited = favoriteCharacters.includes(characterName);
+
+if (!isAlreadyFavorited) {
+    favoriteCharacters.push(characterName);
+
+    // Mostrar personaje favorito en la sección de favoritos
+    const favoriteContainer = document.getElementById('favoriteCharactersContainer');
+    const favoriteCharacterHTML = `
+      <div class="favoriteCharacter">
+        <h3 class="characterName">${characterName}</h3>
+        <img class="characterImage" src="${character.imageUrl}" alt="${character.name}">
+      </div>
+    `;
+    favoriteContainer.innerHTML += favoriteCharacterHTML;
+  }
+}
+
 //FUNCION PARA PODER FILTRAR PERSONAJES
 
 document.addEventListener('DOMContentLoaded', function () {
     const searchButton = document.querySelector('.searchButton');
 
     searchButton.addEventListener('click', function (event) {
-        event.preventDefault(); // Evitar que el formulario se envíe
+        event.preventDefault(); // Evitar que se recargue la página involuntariamente
 
         const filterInput = document.getElementById('filterInput');
         const searchTerm = filterInput.value.toLowerCase();
